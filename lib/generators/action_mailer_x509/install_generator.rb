1f0c6079c42a3638106940f1eb270aec712a0d74
Jenua Boiko <jeyboy@bigmir.net>
2013-08-09 10:59:13 +0300

    fix install generator - bump

--- lib/generators/action_mailer_x509/install_generator.rb
+++ lib/generators/action_mailer_x509/install_generator.rb
@@ -2,6 +2,8 @@ module ActionMailerX509
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../', __FILE__)
+
+      desc 'Copy settings'
       def copy_initializer
         template 'templates/x509_settings.rb', 'config/initializers/x509_settings.rb'
       end
