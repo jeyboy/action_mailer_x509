ef6ea5d4bf7554f480fad70f2a81b87d784523da
Jenua Boiko <jeyboy1985@gmail.com>
2013-09-05 08:30:30 +0400

    add tests for p12 generation

--- spec/adds_spec.rb
+++ spec/adds_spec.rb
@@ -7,6 +7,28 @@ describe 'Test additional functions' do
         p12_bytes = SecurityObject.p12_certificate({:password => 'demo'})
         lambda { OpenSSL::PKCS12.new p12_bytes, 'demo' }.should_not raise_error
       end
+
+      it 'Must generate p12 with params' do
+        subject_settings = {
+            :country => 'TestCountry',
+            :state => 'TestState',
+            :location => 'TestLocation',
+            :organization => 'TestOrganization',
+            :organizational_unit => 'TestOrganizationalUnit',
+            :common_name => 'TestCommonName',
+            :email => 'a@b.com'
+        }
+        p12_bytes = SecurityObject.p12_certificate(
+            :password => 'demo',
+            :subject => subject_settings
+        )
+        p12 = OpenSSL::PKCS12.new p12_bytes, 'demo'
+
+        subject_attrs = p12.certificate.subject.to_a
+        subject_attrs.each do |attr|
+          subject_settings[SecurityObject::ATTRS[attr.first]].should eql attr[1]
+        end
+      end
     end
   end
 
